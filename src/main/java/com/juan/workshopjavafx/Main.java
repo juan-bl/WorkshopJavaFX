de7256f828b0c8707fd10b314abd30ff6b5448f8
Juan Nícolas <juannicolasdev@gmail.com>
2026-05-04 20:11:35 -0300

    feat: criando menu no topo da janela

--- src/main/java/com/juan/workshopjavafx/Main.java
+++ src/main/java/com/juan/workshopjavafx/Main.java
@@ -3,6 +3,7 @@ package com.juan.workshopjavafx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,10 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        ScrollPane scrollPane = fxmlLoader.load();
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+        Scene scene = new Scene(scrollPane);
         stage.setTitle("Workshop JavaFX");
         stage.setScene(scene);
         stage.show();
